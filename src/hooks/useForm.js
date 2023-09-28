a3364578a13720c77c0b9ecd81ec703123191b1b
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-28 18:19:09 -0300

    validate input number

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,16 +1,11 @@
 import { useState } from 'react'
 
 export function useForm() {
-  const [inputValue, setInputValue] = useState('1')
-
   const validateNumberInput = (value) => {
     if (value.startsWith(' ')) return
-    setInputValue(value.replace(/[^0-9]/g, ''))
+
+    return value.replace(/[^0-9]/g, '')
   }
 
-  const resetForm = () => {
-    setInputValue('')
-  }
-
-  return { inputValue, validateNumberInput, resetForm }
+  return { validateNumberInput }
 }
