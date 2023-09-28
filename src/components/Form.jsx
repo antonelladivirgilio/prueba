3c6e67e5090c085dadcb21b837d5563a8f63622a
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-28 18:20:29 -0300

    refactor form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,44 +1,113 @@
-import { useId } from 'react'
+import { useEffect, useId, useState } from 'react'
 import { useForm } from '../hooks/useForm'
 import { useCurrencies } from '../hooks/useCurrencies'
+import { getCurrenciesRate } from '../services/getCurrenciesRate'
 
 export function Form() {
   const id = useId()
-  const { inputValue, validateNumberInput } = useForm()
+  const { validateNumberInput } = useForm()
+
+  const [amount, setAmount] = useState('1')
+  const [from, setFrom] = useState({
+    code: 'USD',
+    rate: 0
+  })
+  const [to, setTo] = useState({ code: 'EUR', rate: 0 })
+  const [finalAmount, setFinalAmount] = useState(0)
   const { currencies } = useCurrencies()
+
+  const handleChangeAmount = (event) => {
+    const { value } = event.target
+    setAmount(validateNumberInput(value))
+  }
+
+  const handleChangeFrom = (event) => {
+    setFrom({
+      ...from,
+      code: event.target.value
+    })
+  }
+
+  const handleChangeTo = (event) => {
+    setTo({
+      ...to,
+      code: event.target.value
+    })
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-  }
+    //algo
 
-  const handleInputChange = (event) => {
-    const { value } = event.target
-    validateNumberInput(value)
+    const updateRates = async () => {
+      const rates = await getCurrenciesRate({ from: from.code, to: to.code })
+      return rates
+    }
+
+    if (from.code !== to.code) {
+      updateRates().then((response) => {
+        setFrom({
+          ...from,
+          rate: response[from.code]
+        })
+        setTo({
+          ...to,
+          rate: response[to.code]
+        })
+        setFinalAmount(((amount * from.rate) / to.rate).toFixed(2))
+      })
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor={`${id}-amount`}>Amount</label>
-      <input
-        type="text"
-        id={`${id}-amount`}
-        value={inputValue}
-        onChange={handleInputChange}
-      />
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={`${id}-amount`}>Amount</label>
+        <input
+          type="text"
+          name="amount"
+          id={`${id}-amount`}
+          value={amount}
+          onChange={handleChangeAmount}
+        />
 
-      <label htmlFor={`${id}-from-currency`}>From</label>
-      <select name="from-currency" id={`${id}-from-currency`}>
-        {currencies.map((currency) => (
-          <option key={currency.code}>{currency.name}</option>
-        ))}
-      </select>
+        <label htmlFor={`${id}-from-currency`}>From</label>
+        <select
+          name="from"
+          id={`${id}-from-currency`}
+          value={from.code}
+          onChange={handleChangeFrom}
+        >
+          {currencies.map((currency) => (
+            <option key={`from-${currency.code}`} value={currency.code}>
+              {currency.name}
+            </option>
+          ))}
+        </select>
 
-      <label htmlFor={`${id}-to-currency`}>To</label>
-      <select name="to-currency" id={`${id}-to-currency`}>
-        {currencies.map((currency) => (
-          <option key={currency.code}>{currency.name}</option>
-        ))}
-      </select>
-    </form>
+        <label htmlFor={`${id}-to-currency`}>To</label>
+        <select
+          name="to"
+          id={`${id}-to-currency`}
+          value={to.code}
+          onChange={handleChangeTo}
+        >
+          {currencies.map((currency) => (
+            <option key={`to-${currency.code}`} value={currency.code}>
+              {currency.name}
+            </option>
+          ))}
+        </select>
+
+        <button>convertir</button>
+      </form>
+
+      <h1>
+        {amount} {from.code} = {finalAmount} {to.code}
+      </h1>
+      <p>
+        1 {from.code} = {to.rate} {to.code}
+      </p>
+    </>
   )
 }
