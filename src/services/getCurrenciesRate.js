d396792a03b5b3fdb641887d77b4480087dd072d
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-28 23:13:00 -0300

    Add getRates service

--- src/services/getCurrenciesRate.js
+++ src/services/getCurrenciesRate.js
@@ -6,3 +6,13 @@ export const getCurrenciesRate = async ({ fromCurrency, toCurrency }) => {
   const { rates } = await response.json()
   return rates
 }
+
+export const getRates = async ({ baseCurrency }) => {
+  const response = await fetch(
+    `https://api.vatcomply.com/rates?base=${baseCurrency}`
+  )
+
+  const { rates } = await response.json()
+
+  return rates
+}
