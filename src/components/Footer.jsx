5bd48d95b8565b6740268bda8ce7208ac507ebeb
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-30 12:15:40 -0300

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from './Footer.module.css'
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Euro to US Dollar conversion — Last updated Dec 15, 2022, 19:17 UTC
+    </footer>
+  )
+}
