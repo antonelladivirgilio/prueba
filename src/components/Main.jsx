584c2448ad07accfb50bce783f6bfa347cde82d1
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-30 18:13:53 -0300

    Refactor Main
    Get values from context

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,20 @@
+import { useCurrencyStore } from '../hooks/useCurrencyStore'
 import { Footer } from './Footer'
 import styles from './Main.module.css'
 
-export function Main({ bannerContent, children }) {
+export function Main({ children }) {
+  const { amount, baseCurrency, convertTo, currencies } = useCurrencyStore()
+
+  const baseCurrencyName = currencies[baseCurrency]?.name
+  const convertToName = currencies[convertTo]?.name
+
   return (
     <main className={styles.card}>
       <div className={styles.card__banner}>
         <div>
-          <h2 className={styles.card__title}>{bannerContent}</h2>
+          <h2 className={styles.card__title}>
+            {`${amount} ${baseCurrency} to ${convertTo} - Convert ${baseCurrencyName} to ${convertToName}`}
+          </h2>
         </div>
       </div>
       <div className={styles.card__content}>{children}</div>
