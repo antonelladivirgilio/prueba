383fc4e9606aac9b3d9c8351cc003a1db3d4d818
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-30 18:07:27 -0300

    Refactor getRates service
    Add try catch

--- src/services/getRates.js
+++ src/services/getRates.js
@@ -1,9 +1,12 @@
 import { API_GET_RATES_BY_BASE } from '../contants'
 
 export const getRates = async ({ baseCurrency }) => {
-  const response = await fetch(`${API_GET_RATES_BY_BASE}${baseCurrency}`)
+  try {
+    const response = await fetch(`${API_GET_RATES_BY_BASE}${baseCurrency}`)
+    const { rates } = await response.json()
 
-  const { rates } = await response.json()
-
-  return rates
+    return rates
+  } catch (error) {
+    throw new Error('Error getting rates')
+  }
 }
