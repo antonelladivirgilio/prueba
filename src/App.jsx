74b83f3819b4cd4665bf859371134d7c6adb2dc2
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-30 15:04:04 -0300

    Fix remove unused component import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import styles from './App.module.css'
-import { Footer } from './components/Footer'
 import { Form } from './components/Form'
 import { Header } from './components/Header'
 import { Main } from './components/Main'
