18e9f54012f99b1c718a74917d2d307f6f4ef1b0
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-10-02 18:32:01 -0300

    Test e2e

--- cypress/e2e/currency_exchange_app.cy.js
+++ cypress/e2e/currency_exchange_app.cy.js
@@ -15,4 +15,33 @@ describe('currency exchange app', () => {
 
     cy.contains('1 USD = ')
   })
+
+  it('user can get the currency exchange', () => {
+    cy.get('input[name=amount]').clear()
+    cy.get('input[name=amount]').type('100')
+    cy.get('select[name=baseCurrency]').select('EUR')
+    cy.get('select[name=convertTo]').select('JPY')
+
+    cy.contains('100 Euro =')
+  })
+
+  it('user can not get the currency exchange', () => {
+    cy.get('input[name=amount]').clear()
+    cy.get('input[name=amount]').type('100')
+    cy.get('select[name=baseCurrency]').select('HRK')
+    cy.get('select[name=convertTo]').select('EUR')
+
+    cy.contains('No se pudo realizar la conversión')
+  })
+
+  it('user can swap the currency exchange usd to eur', () => {
+    cy.get('input[name=amount]').clear()
+    cy.get('input[name=amount]').type('100')
+    cy.get('select[name=baseCurrency]').select('EUR')
+    cy.get('select[name=convertTo]').select('JPY')
+    cy.get('button').click()
+
+    cy.get('select[name=baseCurrency]').should('have.value', 'JPY')
+    cy.get('select[name=convertTo]').should('have.value', 'EUR')
+  })
 })
