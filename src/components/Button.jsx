72787801d708757a8fb7dfbe6a3b4cffcf422a75
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-10-02 10:04:02 -0300

    Refactor Button component
    Set disabled false as default

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import styles from './Button.module.css'
 
-export function Button({ children, onClick, disabled }) {
+export function Button({ children, onClick, disabled = false }) {
   return (
     <button onClick={onClick} className={styles.button} disabled={disabled}>
       {children}
