25319555982da2fb61c08d18f0bb51974fce41e8
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-10-02 14:50:51 -0300

    Hotfix Main styles
    Change subgrid to grid areas for the browser support

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,16 +10,18 @@ export function Main({ children }) {
 
   return (
     <main className={styles.card}>
-      <div className={styles.card__banner}>
-        <div>
-          <h2 className={styles.card__title}>
-            {`${amount} ${baseCurrency} to ${convertTo} - Convert ${baseCurrencyName} to ${convertToName}`}
-          </h2>
-        </div>
+      <div className={styles.card__banner}></div>
+
+      <div className={styles.card__title}>
+        <h2>
+          {`${amount} ${baseCurrency} to ${convertTo} - Convert ${baseCurrencyName} to ${convertToName}`}
+        </h2>
       </div>
+
       <div className={styles.card__content}>{children}</div>
+
       <div className={styles.card__footer}>
-        <div>
+        <div className={styles.footer__content}>
           <Footer />
         </div>
       </div>
